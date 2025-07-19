9dec3b130d1b296fac754ee3e903276f47552899
FlowerYuanL <3152739308@qq.com>
2025-07-19 17:29:43 +0800

    study end by sql

--- src/main/java/com/example/utils/MyBatisUtils.java
+++ src/main/java/com/example/utils/MyBatisUtils.java
@@ -1,15 +1,29 @@
 package com.example.utils;
 
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
-import java.io.InputStream;
-/*
+import java.io.IOException;
+import java.io.Reader;
+
 public class MyBatisUtils {
-    private static SqlSessionFactory sqlSessionFactory;
-
-    static{
-        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
-        sqlSessionFactory
+    private static SqlSessionFactory sqlSessionFactory = null;
+    static {
+        //创建sqlSession
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
-}*/
+    public static SqlSession getSession(){
+        if (sqlSessionFactory == null){
+            System.out.println("error: sqlSessionFactory is null");
+        }
+        return sqlSessionFactory.openSession();
+    }
+
+}
--- src/test/java/com/example/mapper/UserAndJobMapperTest.java
+++ src/test/java/com/example/mapper/UserAndJobMapperTest.java
@@ -3,6 +3,7 @@ package com.example.mapper;
 import com.example.pojo.Job;
 import com.example.pojo.User;
 
+import com.example.utils.MyBatisUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -19,18 +20,20 @@ public class UserAndJobMapperTest {
 
     UserAndJobMapper userAndJobMapper;
     SqlSession sqlSession=null;
+    MyBatisUtils myBatisUtils = new MyBatisUtils();
 
     @Before
     public void setUp() throws Exception {
-        //1.获取资源核心配置文件
+        /*//1.获取资源核心配置文件
         InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
 
         //2.创建SqlSessionFactory工厂对象（由SqlSessionFactoryBuilder()创建）
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
 
         //3.创建SqlSession对象
-        sqlSession = sqlSessionFactory.openSession();
+        sqlSession = sqlSessionFactory.openSession();*/
 
+        sqlSession = myBatisUtils.getSession();
         //4.获取接口的动态代理对象
         //执行方法时，要找哪一条SQL语句来执行？sql语句的唯一标识是由namespace+statementId
         userAndJobMapper = sqlSession.getMapper(UserAndJobMapper.class);
